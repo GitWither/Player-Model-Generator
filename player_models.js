13cedb4a1518186e611d543cc5a6d283b7896f06
Wither <wither.tweets@gmail.com>
2018-11-24 23:26:11 +0100

    stuff

--- player_models.js
+++ player_models.js
@@ -58,7 +58,7 @@ function setDisplay() {
 }
 
 function generateSteve(secondLayer) {
-    Undo.initEdit({cubes: Blockbench.elements, outliner: true })
+    Undo.initEdit({cubes: Blockbench.elements, outliner: true });
     steveGroup = new Group('steve').addTo('root');
  
     var steveHead = new Cube({name:'head',from:[4,23.5,4],to:[12,31.5,12],autouv:0,faces:{north:{uv:[2,2,4,4]},south:{uv:[6,2,8,4]},west:{uv:[4,2,6,4]},east:{uv:[0,2,2,4]},up:{uv:[4,2,2,0]},down:{uv:[6,0,4,2]}}}).addTo(steveGroup);
@@ -88,67 +88,67 @@ function generateSteve(secondLayer) {
         var steveLeftLeg2 = new Cube({name:'left_leg_2nd_layer',from:[3.5,-1,5.5],to:[8.5,12,10.5],autouv:0,faces:{north:{uv:[1,13,2,16]},south:{uv:[3,13,4,16]},west:{uv:[2,13,3,16]},east:{uv:[0,13,1,16]},up:{uv:[2,12,1,11]},down:{uv:[3,12,2,13]}}}).addTo(steveGroup);
         elements.push(steveLeftLeg2);
     }
-    setDisplay()
-    Canvas.updateAll()
-    steveGroup.openUp().select()
-    playerModelSettings.hide()
-    Undo.finishEdit('Generated Steve Statue')
+    setDisplay();
+    Canvas.updateAll();
+    steveGroup.openUp().select();
+    playerModelSettings.hide();
+    Undo.finishEdit('Generated Steve Statue');
 }
 
 function generateAlex(secondLayer) {
-    Undo.initEdit({cubes: Blockbench.elements, outliner: true})
-    alexGroup = new Group('alex').addTo('root')
+    Undo.initEdit({cubes: Blockbench.elements, outliner: true});
+    alexGroup = new Group('alex').addTo('root');
 
     var alexHead = new Cube({name:'head',from:[4,23.5,4],to:[12,31.5,12],autouv:0,faces:{north:{uv:[2,2,4,4]},south:{uv:[6,2,8,4]},west:{uv:[4,2,6,4]},east:{uv:[0,2,2,4]},up:{uv:[4,2,2,0]},down:{uv:[6,0,4,2]}}}).addTo(alexGroup);
-    elements.push(alexHead)
+    elements.push(alexHead);
     var alexBody = new Cube({name:'body',from:[4,11.5,6],to:[12,23.5,10],autouv:0,faces:{north:{uv:[5,5,7,8]},south:{uv:[8,5,10,8]},west:{uv:[7,5,8,8]},east:{uv:[4,5,5,8]},up:{uv:[7,5,5,4]},down:{uv:[9,4,7,5]}}}).addTo(alexGroup);
-    elements.push(alexBody)
+    elements.push(alexBody);
     var alexRightArm = new Cube({name:'right_arm',from:[12,11.5,6],to:[15,23.5,10],autouv:0,faces:{north:{uv:[11,5,11.75,8]},south:{uv:[12.75,5,13.5,8]},west:{uv:[11.75,5,12.75,8]},east:{uv:[10,5,11,8]},up:{uv:[11.75,5,11,4]},down:{uv:[12.5,4,11.75,5]}}}).addTo(alexGroup);
-    elements.push(alexRightArm)
+    elements.push(alexRightArm);
     var alexLeftArm = new Cube({name:'left_arm',from:[1,11.5,6],to:[4,23.5,10],autouv:0,faces:{north:{uv:[9,13,9.75,16]},south:{uv:[10.75,13,11.5,16]},west:{uv:[9.75,13,10.75,16]},east:{uv:[8,13,9,16]},up:{uv:[9.75,13,9,12]},down:{uv:[10.5,12,9.75,13]}}}).addTo(alexGroup);
-    elements.push(alexLeftArm)
+    elements.push(alexLeftArm);
     var alexRightLeg = new Cube({name:'right_leg',from:[4,-0.5,6],to:[8,11.5,10],autouv:0,faces:{north:{uv:[5,13,6,16]},south:{uv:[7,13,8,16]},west:{uv:[6,13,7,16]},east:{uv:[4,13,5,16]},up:{uv:[6,13,5,12]},down:{uv:[7,12,6,13]}}}).addTo(alexGroup);
-    elements.push(alexRightLeg)
+    elements.push(alexRightLeg);
     var alexLeftLeg = new Cube({name:'left_leg',from:[8,-0.5,6],to:[12,11.5,10],autouv:0,faces:{north:{uv:[1,5,2,8]},south:{uv:[3,5,4,8]},west:{uv:[2,5,3,8]},east:{uv:[0,5,1,8]},up:{uv:[2,5,1,4]},down:{uv:[3,4,2,5]}}}).addTo(alexGroup);
-    elements.push(alexLeftLeg)
+    elements.push(alexLeftLeg);
 
     if (secondLayer) {
         var alexHead2 = new Cube({name:'head_2nd_layer',from:[3.5,23,3.5],to:[12.5,32,12.5],autouv:0,faces:{north:{uv:[10,2,12,4]},south:{uv:[14,2,16,4]},west:{uv:[12,2,14,4]},east:{uv:[8,2,10,4]},up:{uv:[12,2,10,0]},down:{uv:[14,0,12,2]}}}).addTo(alexGroup);
-        elements.push(alexHead2)
+        elements.push(alexHead2);
         var alexBody2 = new Cube({name:'body_2nd_layer',from:[4,11.02,5.49],to:[12,24.03,10.49],autouv:0,faces:{north:{uv:[5,9,7,12]},south:{uv:[8,9,10,12]},west:{uv:[7,9,8,12]},east:{uv:[4,9,5,12]},up:{uv:[7,9,5,8]},down:{uv:[9,8,7,9]}}}).addTo(alexGroup);
-        elements.push(alexBody2)
+        elements.push(alexBody2);
         var alexRightArm2 = new Cube({name:'right_arm_2nd_layer',from:[11.5,11,5.5],to:[15.5,24,10.5],autouv:0,faces:{north:{uv:[10.75,9,11.5,12]},south:{uv:[12.75,9,13.5,12]},west:{uv:[11.75,9,12.75,12]},east:{uv:[10,9,11,12]},up:{uv:[11.75,9,11,8]},down:{uv:[12.5,8,11.75,9]}}}).addTo(alexGroup);
-        elements.push(alexRightArm2)
+        elements.push(alexRightArm2);
         var alexLeftArm2 = new Cube({name:'left_arm_2nd_layer',from:[0.5,11,5.5],to:[4.5,24,10.5],autouv:0,faces:{north:{uv:[13,13,13.75,16]},south:{uv:[14.75,13,15.5,16]},west:{uv:[13.75,13,14.75,16]},east:{uv:[12,13,13,16]},up:{uv:[13.75,13,13,12]},down:{uv:[14.5,12,13.75,13]}}}).addTo(alexGroup);
-        elements.push(alexLeftArm2)
+        elements.push(alexLeftArm2);
         var alexRightLeg2 = new Cube({name:'right_leg_2nd_layer',from:[7.5,-1,5.5],to:[12.5,12,10.5],autouv:0,faces:{north:{uv:[1,9,2,12]},south:{uv:[3,9,4,12]},west:{uv:[0,9,1,12]},east:{uv:[0,9,1,12]},up:{uv:[2,9,1,8]},down:{uv:[3,8,2,9]}}}).addTo(alexGroup);
-        elements.push(alexRightLeg2)
+        elements.push(alexRightLeg2);
         var alexLeftLeg2 = new Cube({name:'left_leg_2nd_layer',from:[3.5,-1,5.5],to:[8.5,12,10.5],autouv:0,faces:{north:{uv:[1,13,2,16]},south:{uv:[3,13,4,16]},west:{uv:[2,13,3,16]},east:{uv:[0,13,1,16]},up:{uv:[2,13,1,12]},down:{uv:[3,12,2,13]}}}).addTo(alexGroup);
-        elements.push(alexLeftLeg2)
+        elements.push(alexLeftLeg2);
     }
-    setDisplay()
-    Canvas.updateAll()
-    alexGroup.openUp().select()
-    playerModelSettings.hide()
-    Undo.finishEdit('Generated Alex Statue')
+    setDisplay();
+    Canvas.updateAll();
+    alexGroup.openUp().select();
+    playerModelSettings.hide();
+    Undo.finishEdit('Generated Alex Statue');
 }
 
 function generateCape(){
-    Undo.initEdit({cubes: Blockbench.elements, outliner: true})
-    var cape = new Cube({name:'cape',from:[2.5,6,13],to:[13.5,23,14],autouv:0,faces:{north:{uv:[0.5,0.5,6,9]},south:{uv:[6.5,0.5,12,9]},west:{uv:[6,0.5,6.5,9]},east:{uv:[0,0.5,0.5,9]},up:{uv:[6,0.5,0.5,0]},down:{uv:[11.5,0,6,0.5]}},rotation:[-22.5,0,0],origin:[7.5,14,10.5]}).addTo()
-    elements.push(cape)
-    Canvas.updateAll()
-    setDisplay()
-    Undo.finishEdit('Generated Cape Model')
+    Undo.initEdit({cubes: Blockbench.elements, outliner: true});
+    var cape = new Cube({name:'cape',from:[2.5,6,13],to:[13.5,23,14],autouv:0,faces:{north:{uv:[0.5,0.5,6,9]},south:{uv:[6.5,0.5,12,9]},west:{uv:[6,0.5,6.5,9]},east:{uv:[0,0.5,0.5,9]},up:{uv:[6,0.5,0.5,0]},down:{uv:[11.5,0,6,0.5]}},rotation:[-22.5,0,0],origin:[7.5,14,10.5]}).addTo();
+    elements.push(cape);
+    Canvas.updateAll();
+    setDisplay();
+    Undo.finishEdit('Generated Cape Model');
 }
 
 playerModelSettings.onConfirm = function() {
     switch($('#player_model_list')[0].value) {
         case 'steve':
-            generateSteve($('#second_layer')[0].checked)
+            generateSteve($('#second_layer')[0].checked);
             break;
         case 'alex':
-            generateAlex($('#second_layer')[0].checked)
+            generateAlex($('#second_layer')[0].checked);
             break;
         case 'cape':
             playerModelSettings.hide();
@@ -163,11 +163,11 @@ playerModelSettings.onConfirm = function() {
 }
 
 capeInfo.onConfirm = function() {
-    playerModelSettings.hide()
-    capeInfo.hide()
-    generateCape()
+    playerModelSettings.hide();
+    capeInfo.hide();
+    generateCape();
 }
 
 onUninstall = function() {
-	MenuBar.removeAction('filter.generate_player_statue')
+	MenuBar.removeAction('filter.generate_player_statue');
 }
